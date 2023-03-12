8562048269a5ac9130ab8c5f114a363bd05c7348
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-03-12 09:51:06 +0300

    HTMLs

--- boardgamer_store/settings.py
+++ boardgamer_store/settings.py
@@ -123,4 +123,6 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+STATIC_ROOT = 'static'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
