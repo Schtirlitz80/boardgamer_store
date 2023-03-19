99b5972183990828edf56356ebbd9b07bb58c2f6
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-03-19 12:46:31 +0300

    Changed navbar, added 'basket' to the navbar

---
+++ static/js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function (){
+    let form = $('#form_buying_product');
+    console.log(form);
+    form.on('submit', function (e){
+        e.preventDefault();
+        let nmb = $('#number').val();
+        console.log('Quantity: ', nmb);
+        let submit_btn = $('#submit_btn');
+        let product_id = submit_btn.data("product_id");
+        let product_name = submit_btn.data("name");
+        let product_price = submit_btn.data("price");
+        console.log('product: ', product_id);
+        console.log('product name: ', product_name);
+        console.log('product price: ', product_price);
+
+        $('.basket-items ul').append('<li class="basket-items"><a class="dropdown-item" href="#">Арт.'+
+            product_id + ' <b>' + product_name + '</b> - ' + nmb + ' шт. * ' + product_price + ' руб.</a>' +
+            '<button type="button" class="btn-close delete-item" aria-label="Close"></button></li>')
+    })
+});
