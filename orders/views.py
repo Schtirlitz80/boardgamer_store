662cac45ed58693cd25b054d33e9321ebeac1412
Schtirlitz80 <Schtirlitz80@mail.ru>
2023-03-29 01:11:43 +0300

    Fixed some bugs with basket

--- orders/views.py
+++ orders/views.py
@@ -1,28 +1,38 @@
-from django.shortcuts import render
 from django.http import JsonResponse
 from .models import ProductInBasket
 
+
 # Create your views here.
-def basket_adding(request):
+def basket_adding(request):     # request.POST получаем из $.ajax({})
     return_dict = dict()
     session_key = request.session.session_key
     data = request.POST
-    print(data)
     product_id = data.get("product_id")
     nmb = data.get("nmb")
+    is_delete = data.get("is_delete")
 
-    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, defaults={"nmb": nmb})
-    if not created:
-        new_product.nmb += int(nmb)
-        new_product.save(force_update=True)
+    if is_delete == 'true':
+        ProductInBasket.objects.filter(product_id=product_id).update(is_active=False)
+    else:
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key,
+                                                                     product_id=product_id,
+                                                                     is_active=True,
+                                                                     defaults={"nmb": nmb})
+        if not created:
+            new_product.nmb += int(nmb)
+            new_product.save(force_update=True)
 
+    # common code for both cases
     products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
     products_total_nmb = products_in_basket.count()
     return_dict["products_total_nmb"] = products_total_nmb
+
     return_dict["products"] = list()
 
     for item in products_in_basket:
         product_dict = dict()
+        product_dict["id"] = item.id
+        product_dict["product_id"] = item.product_id
         product_dict["name"] = item.product.name
         product_dict["price_per_item"] = item.price_per_item
         product_dict["nmb"] = item.nmb
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,45 +1,61 @@
 $(document).ready(function (){
+    console.log('j01. document.ready')
     let form = $('#form_buying_product');
-    console.log(form);
+    let form2 = $('#nav_basket');
 
-    form.on('submit', function (e){
-        e.preventDefault();
-        let nmb = $('#number').val();
-        console.log('Quantity: ', nmb);
-        let submit_btn = $('#submit_btn');
-        let product_id = submit_btn.data("product_id");
-        let product_name = submit_btn.data("name");
-        let product_price = submit_btn.data("price");
-        console.log('product: ', product_id);
-        console.log('product name: ', product_name);
-        console.log('product price: ', product_price);
 
-        let data = {};
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
+            }
+        }
+        return cookieValue;
+    }
+
+    const csrf_token = getCookie('csrftoken');
+
+
+    function basketUpdating(product_id, nmb, is_delete){
+        let data = {};  // Данные, которые будем передавать в POST запросе
         data.product_id = product_id;
         data.nmb = nmb;
-
-        let csrf_token = $('#form_buying_product [name="csrfmiddlewaretoken"]').val();
         data["csrfmiddlewaretoken"] = csrf_token;
 
-        let url = form.attr("action");
+        if (is_delete){
+            data["is_delete"] = true;
+        }
+        console.log('3data: ', data)
 
-    // console.log(data)
+        let url = form.data("action"); // Адрес, на который ajax будет отправлять POST-запрос
+        console.log(typeof url)
+        if (url == undefined){
+            url = form2.data('action');
+        };
+
+        // console.log(data)
         $.ajax({
-            url: url,
-            type: 'POST',
-            data: data,
+            url: url, // куда отправляем запрос
+            type: 'POST', // тип запроса
+            data: data,     // данные, которые мы будем отправлять
             cache: true,
-            success: function (data) {
-                console.log('OK');
-                console.log(data.products_total_nmb);
-                if (data.products_total_nmb){
+            success: function (data) {      // функция, которая вызывается, если получен ответ от сервера
+                //TODO: Почему-то одинаковые артикулы в раскрывающемся списке.
+                if (data.products_total_nmb || data.products_total_nmb == 0){
                     $('#basket_total_nmb').text('(' + data.products_total_nmb + ')');
-                    console.log(data.products);
                     $('.basket-items ul').html('');
                     $.each(data.products, function (k, v){
                         $('.basket-items ul').append('<li class="basket-items"><a class="dropdown-item" href="#">Арт.'+
                             product_id + ' <b>' + v.name + '</b> - ' + v.nmb + ' шт. * ' + v.price_per_item + ' руб.</a>' +
-                            '<button type="button" class="btn-close delete-item" aria-label="Close"></button></li>')
+                            '<button type="button" class="btn-close delete-item" aria-label="Close" data-product_id="' +
+                            v.product_id + '"></button></li>')
                     });
                 }
             },
@@ -47,9 +63,24 @@ $(document).ready(function (){
                 console.log("error")
             }
         })
+    }
+
+    //Срабатывает при нажатии кнопки "Купить" в форме с товаром
+    form.on('submit', function (e){
+        e.preventDefault();
+        let nmb = $('#number').val();
+        let submit_btn = $('#submit_btn');
+        let product_id = submit_btn.data("product_id");
+        let product_name = submit_btn.data("name");
+        let product_price = submit_btn.data("price");
+
+        basketUpdating(product_id, nmb, is_delete=false);
     });
 
-    $(document).on('click', '.delete-item', function (){
-        $(this).closest('li').remove();
+    $(document).on('click', '.delete-item', function(e){
+        e.preventDefault();
+        product_id = $(this).data("product_id");
+        nmb = 0;
+        basketUpdating(product_id, nmb, is_delete=true);
     });
 });
